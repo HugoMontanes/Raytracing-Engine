f00802bc13396a875438c52c533daca109c121ad
Hugo Montañés García <hugomontanes3005@gmail.com>
2025-05-12 01:12:58 +0200

    Update Starter.cpp

--- ray-tracing-engine-main/engine/code/sources/Starter.cpp
+++ ray-tracing-engine-main/engine/code/sources/Starter.cpp
@@ -7,11 +7,13 @@
 
 #define SDL_MAIN_HANDLED
 
+#include <iostream>
 #include <mutex>
 #include <engine/Starter.hpp>
 #include <SDL3/SDL_main.h>
 #include <algorithm>
 #include <cstddef>
+
 #include "engine/Thread_Pool_Manager.hpp"
 
 namespace udit::engine
@@ -45,12 +47,18 @@ namespace udit::engine
         size_t hardware_threads = std::thread::hardware_concurrency();
         unsigned rendering_threads = std::max<size_t>(1u, hardware_threads - 1);
         unsigned input_threads = 1;
+        unsigned loading_threads = 1;
         unsigned general_threads = 1;
+
+        std::cout << "Number of rendering_threads: " << rendering_threads << std::endl;
+        std::cout << "Number of input_threads: " << input_threads << std::endl;
+        std::cout << "Number of loading_threads: " << loading_threads << std::endl;
+        std::cout << "Number of general_threads: " << general_threads << std::endl;
 
         Thread_Pool_Manager::get_instance().initialize(
             general_threads,
             rendering_threads,
-            1,
+            loading_threads,
             input_threads
         );
 
